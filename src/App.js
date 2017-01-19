f39f542f6fafe79eeed7482d2ff5707649016410
Walter Berggren <walter.berggren@gmail.com>
2017-01-19 18:06:12 +0200

    Add skeleton for chat list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Chats from './Chats'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Chats />
       </div>
     );
   }
---
+++ src/Chats.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Chats extends Component {
+  render() {
+    return (
+      <div>Chats will appear here</div>
+    );
+  }
+}
+
+export default Chats;
