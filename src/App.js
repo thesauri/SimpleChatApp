bf3b09297339830c6814d1fcd311219e3e192df9
Walter Berggren <walter.berggren@gmail.com>
2017-01-19 18:13:06 +0200

    Add mock data for the chat list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import ChatList from './ChatList'
 import './App.css';
 
+const chatListMock = [
+  { name: "Chat 1" },
+  { name: "Chat 2" },
+  { name: "Chat 3" }
+]
+
 class App extends Component {
   render() {
     return (
