590be239c91663d1d679be219af9dfa0dae102c0
Walter Berggren <walter.berggren@gmail.com>
2017-01-19 18:24:18 +0200

    Add skeleton for the chatbox

---
+++ src/ChatBoxBubble.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ChatBoxBubble extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ChatBoxBubble;
