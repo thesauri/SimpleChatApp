868747f0203946cca2cdde228aef3817681ab870
Walter Berggren <walter.berggren@gmail.com>
2017-01-20 17:54:13 +0200

    Remove unused variables

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Routes from './Routes';
 import { browserHistory } from 'react-router';
 import './index.css';
-
-const styles = {
-  height: "100%",
-  margin: 20
-}
 
 ReactDOM.render(
   <Routes history={browserHistory} />,
