a5d6283c1954ea25830a7c77083995033d4de845
Walter Berggren <walter.berggren@gmail.com>
2017-01-22 18:31:06 +0200

    Send message to the server when the send button is pressed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { connected: false };
+    this.state = {
+      connected: false,
+      nickname: "User"
+    };
   }
 
   componentDidMount() {
@@ -34,20 +37,37 @@ class App extends Component {
     };
   }
 
+  /* Returns the ID of the current chat. If a chat is defined in the URL (e.g. /1)
+     that value will be used, otherwise it will default to 0
+     This data should perhaps be set in the state instead.. */
+  chatId() {
+    return typeof(this.props.params.chatId) !== "undefined" ? parseInt(this.props.params.chatId, 10) : 0;
+  }
+
+  // Sends a message to the current chat
+  sendMessage(text) {
+    const msg = {
+      type: "newMessage",
+      chatId: this.chatId(),
+      nickname: this.state.nickname,
+      text: text
+    };
+    this.ws.send(JSON.stringify(msg));
+  }
+
   componentWillUnmount() {
     this.ws.close();
   }
 
   render() {
-    const chatId = typeof(this.props.params.chatId) !== "undefined" ? parseInt(this.props.params.chatId, 10) : 0;
     if (this.state.connected) {
       return (
         <div style={styles}>
           <ChatList chats={this.state.chats} />
           <ChatBox
-            name={this.state.chats[chatId].name}
-            messages={this.state.chats[chatId].messages} />
-          <TextBox />
+            name={this.state.chats[this.chatId()].name}
+            messages={this.state.chats[this.chatId()].messages} />
+          <TextBox onSendMessage={(text) => this.sendMessage(text)} />
         </div>
       );
     } else {
--- client/src/TextBox.js
+++ client/src/TextBox.js
@@ -9,14 +9,42 @@ const inputStyle = {
 }
 
 class TextBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  textChanged(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  sendMessage(e) {
+    e.preventDefault();
+    this.setState({
+      text: ""
+    });
+    this.props.onSendMessage(this.state.text);
+  }
+
   render() {
     return (
-      <div style={styles}>
-        <input style={inputStyle} type="text" />
-        <button>Send</button>
-      </div>
+      <form style={styles} onSubmit={(e) => this.sendMessage(e)}>
+        <input style={inputStyle} type="text" onChange={(e) => this.textChanged(e)}
+          value={this.state.text} autoFocus />
+        <button type="submit">
+          Send
+        </button>
+      </form>
     );
   }
+}
+
+TextBox.propTypes = {
+  onSendMessage: React.PropTypes.func.isRequired
 }
 
 export default TextBox;
