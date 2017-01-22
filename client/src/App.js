d10545ecd7416c7a45846d6ec8ee73aad11d5481
Walter Berggren <walter.berggren@gmail.com>
2017-01-22 18:58:13 +0200

    Add and show new messages as they arrive

--- client/src/App.js
+++ client/src/App.js
@@ -28,11 +28,33 @@ class App extends Component {
     this.ws.onmessage = (e) => {
       const msg = JSON.parse(e.data);
 
+      switch (msg.type) {
+        case "initial":
+          this.setState({
+            connected: true,
+            chats: msg.data
+          });
+          break;
+
+        case "newMessage":
+          const messageCount = this.state.chats[msg.chatId].messages.length;
+          const newChats = this.state.chats;
+          newChats[msg.chatId].messages.push({
+            id: messageCount + 1,
+            user: msg.nickname,
+            message: msg.text
+          });
+          this.setState({
+            chats: newChats
+          });
+          break;
+
+        default:
+          console.error("Unknown message type: " + msg.type);
+      }
+
       if (msg.type === "initial") {
-        this.setState({
-          connected: true,
-          chats: msg.data
-        });
+
       }
     };
   }
